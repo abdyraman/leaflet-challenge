615bff742a9ecb58075be38c1b748c416f2d895e
abdyraman <abdyramanova.j@gmail.com>
2023-06-03 11:55:09 -0400

    Update logic.js

--- Leaflet challenge_2/static/js/logic.js
+++ Leaflet challenge_2/static/js/logic.js
@@ -2,39 +2,20 @@
 let myMap = L.map('map').setView([36.778259, -119.417931], 3);
 
 // Adding the tile layers
-
-// Satellite tile layer
 var satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri'
 });
 
-// Greyscale tile layer
 var greyscaleLayer = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
   attribution: 'Tiles &copy; Wikimedia'
 });
 
-// Outdoors tile layer
 var outdoorsLayer = L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
   attribution: 'Tiles &copy; Thunderforest'
 });
 
-// Adding the tile layers to the map
-satelliteLayer.addTo(myMap);
-greyscaleLayer.addTo(myMap);
-outdoorsLayer.addTo(myMap);
-
-// Creating a base layers object
-var baseLayers = {
-  'Satellite': satelliteLayer,
-  'Greyscale': greyscaleLayer,
-  'Outdoors': outdoorsLayer
-};
-// Adding layer control to switch between tile layers
-L.control.layers(baseLayers).addTo(myMap);
-
-
-// Creating a merged GeoJSON layer
-var mergedGeoJSON = L.geoJSON();
+// Creating a layer group for GeoJSON data
+var geoJSONLayer = L.layerGroup();
 
 // URLs of the GeoJSON files
 var urls = [
@@ -44,18 +25,35 @@ var urls = [
   'static/data/PB2002_steps.json'
 ];
 
-// Loading and adding each GeoJSON file to the mergedGeoJSON layer
+// Loading and adding each GeoJSON file to the GeoJSON layer group
 urls.forEach(function(url) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
       var geojsonLayer = L.geoJSON(data);
       geojsonLayer.eachLayer(function(layer) {
-        mergedGeoJSON.addLayer(layer);
+        geoJSONLayer.addLayer(layer);
       });
     });
 });
 
-// Adding the merged GeoJSON layer to the map
-mergedGeoJSON.addTo(myMap);
+// Adding the tile layers and GeoJSON layer to the map
+satelliteLayer.addTo(myMap);
+greyscaleLayer.addTo(myMap);
+outdoorsLayer.addTo(myMap);
+geoJSONLayer.addTo(myMap);
 
+// Creating a base layers object
+var baseLayers = {
+  'Satellite': satelliteLayer,
+  'Greyscale': greyscaleLayer,
+  'Outdoors': outdoorsLayer
+};
+
+// Creating an overlays object
+var overlays = {
+  'Tectonic Plates': geoJSONLayer
+};
+
+// Adding layer control to switch between tile layers and GeoJSON layer
+L.control.layers(baseLayers, overlays).addTo(myMap);
